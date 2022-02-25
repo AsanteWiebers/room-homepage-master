f42cf0aaa63f565af87b9ca52e804980e8e4b697
Asante Wiebers <67924684+AsanteWiebers@users.noreply.github.com>
2022-02-25 21:23:30 +0100

    mobile navigation

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
-ScrollReveal().reveal('.logo', { duration: 2000});
-ScrollReveal().reveal('.hamburger-icon', {duration: 2000});
+
+
+
+ScrollReveal({
+    reset: true
+});
+
+ScrollReveal().reveal('.logo', { duration: 2000 });
+ScrollReveal().reveal('.hamburger-icon', { duration: 2000 });
+
+
+ScrollReveal().reveal('.hero_title',
+    { distance: '50px'} ,
+   
+);
+
+ScrollReveal().reveal('.hero_title',
+    { duration: 2500 }
+
+);
+
+
 
 // options - delay, duration , interval 
