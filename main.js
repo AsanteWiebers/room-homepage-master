71ea70fe4b684a5a02c2807281fc12e160e7d6f7
Asante Wiebers <67924684+AsanteWiebers@users.noreply.github.com>
2022-02-28 08:54:54 +0100

    navigation bigger screens

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 
+// mobile menu toggle
 
+function myFunction() {
+    
+    var x = document.getElementById("myLinks");
+    if(x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+// scroll animation 
 
 ScrollReveal({
     reset: true
